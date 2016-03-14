e455ff7fba971f8e0f3e3e04a6c8d1836e5ff4db
aj9045 <identity_unveiled@hotmail.com>
2016-03-14 19:16:52 -0400

    add simple date box component

---
+++ src/components/DateBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class DateBox extends Component {
+  render() {
+    return (
+      <div>
+        show DateBox
+      </div>
+    );
+  }
+}
