84a2fa01e1bcbbae8b315a21158277e2d9990e5a
aj9045 <identity_unveiled@hotmail.com>
2016-03-14 19:20:44 -0400

    delete DateButton

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import DateButton from "./components/DateButton";
 import DateBox from "./components/DateBox";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <DateButton />
         <DateBox />
       </div>
     );
