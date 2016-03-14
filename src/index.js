14a7bd7682c7c00e21962f5ab81934117c5bc7ee
aj9045 <identity_unveiled@hotmail.com>
2016-03-14 19:58:04 -0400

    add func to get next friday 13th

--- src/index.js
+++ src/index.js
@@ -54,6 +54,28 @@ class App extends Component {
     return (month + " " + day + ", " + year);
   };
 
+
+_getNextFriday13() {
+  var currentDate = this._getCurrentDateInfo();
+  var date = currentDate.date;
+  var month = currentDate.month;
+  var numberDate = currentDate.numberDate;
+
+  while (date.getDay() !== 5 ) {
+    if (numberDate < 13) {
+      date.setDate(13);
+      numberDate = 13;
+    }
+    else {
+      date.setMonth(month, 13);
+      month += 1;
+    }
+  }
+
+  return this._getFullDate(date);
+};
+
+
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
