3cc0216ad5ebe7ba1aa0b8f247b1c0ad53788521
aj9045 <identity_unveiled@hotmail.com>
2016-03-14 19:56:25 -0400

    delete dateBox component and set constructor and componentWillMOunt ot get current date and next friday 13th date

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import DateBox from "./components/DateBox";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: "",
+      nextDate: "",
+    };
+  }
+
+  componentWillMount() {
+    var currentDate = this._getFullDate(new Date);
+    var nextDate = this._getNextFriday13();
+    this.setState({
+      currentDate: currentDate,
+      nextDate: nextDate
+    });
+  }
+
   render() {
+    if (!this.state.nextDate) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
-        <DateBox />
+        <div>Today's Date: {this.state.currentDate}</div><div style={{display:"none"}}>'</div>
+        <div>Next Friday the 13th: {this.state.nextDate}</div>
       </div>
     );
   }
+
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
